53488edf6147e123d1c4077ba510408ab9144131
Grady O'Connell <flipcoder@gmail.com>
2016-05-26 00:56:33 -0700

    split launching

--- README.md
+++ README.md
@@ -24,6 +24,15 @@ The active window is expanded as far as possible on the current display
 gridtop fill
 ```
 
+### Split
+
+Split the active window and launch an application in its space
+
+```
+gridtop hsplit <application>
+gridtop vsplit <application>
+```
+
 ## What next?
 
 Check out http://github.com/flipcoder/gridtop-old for a list of things that may be included in the future.
--- gridtop.py
+++ gridtop.py
@@ -3,6 +3,7 @@
 import sys
 import subprocess
 from copy import copy
+import time
 
 BLACKLIST = [
     'Desktop',
@@ -10,6 +11,10 @@ BLACKLIST = [
     'plank',
     'wmctrl -l -G'
 ]
+
+OFFSET = [ 11, 68 ]
+
+DELAY = 1.0
 
 class Window:
     def __init__(self, name, line,):
@@ -48,19 +53,25 @@ def active_window_idx(w):
     return None
     
 windows = []
-lines = subprocess.check_output(['wmctrl', '-G', '-l']).split('\n')
-lines = lines[:-1]
-for i in range(len(lines)):
-    lines[i] = lines[i].split(' ')
-    lines[i] = filter(lambda x: x, lines[i])
-    name = ' '.join(lines[i][7:])
-    lines[i] = lines[i][:6]
-    print name
-    if name in BLACKLIST:
-        continue
-    windows += [Window(name, lines[i])]
-    if windows[-1].num == active_window_num:
-        windows[-1].active = True
+active_window = None
+def update():
+    global windows
+    global active_window
+    lines = subprocess.check_output(['wmctrl', '-G', '-l']).split('\n')
+    lines = lines[:-1]
+    active_window = None
+    for i in range(len(lines)):
+        lines[i] = lines[i].split(' ')
+        lines[i] = filter(lambda x: x, lines[i])
+        name = ' '.join(lines[i][7:])
+        lines[i] = lines[i][:6]
+        if name in BLACKLIST:
+            continue
+        windows += [Window(name, lines[i])]
+        if windows[-1].num == active_window_num:
+            active_window = windows[-1]
+            windows[-1].active = True
+update()
 
 if sys.argv[1]=="left":
     all_windows = sorted(windows, cmp=lambda x,y: x.get_x() - y.get_x())
@@ -108,5 +119,51 @@ elif sys.argv[1]=="down":
         if idx != None:
             windows[idx].activate()
 elif sys.argv[1]=="close":
-    subprocess.call(['xdotool', 'getactivewindow', 'windowkill'])
+    subprocess.call(['xdotool', 'getactivewindow', 'windowunmap'])
+elif sys.argv[1]=="maximize":
+    subprocess.call([
+        'wmctrl', '-r', ':ACTIVE:', '-b', 'toggle,maximized_vert,maximized_horz'
+    ])
+elif sys.argv[1]=="vsplit":
+    p = active_window.pos
+    s = active_window.size
+    subprocess.call([
+        'wmctrl', '-r', ':ACTIVE:', '-e', '0,%s,%s,%s,%s' % (
+            p[0]-OFFSET[0],p[1]-OFFSET[1],
+            s[0],s[1]/2
+        )
+    ])
+    time.sleep(1.0)
+    subprocess.Popen([sys.argv[2]])
+    update()
+    time.sleep(1.0)
+    subprocess.call([
+        'wmctrl', '-r', ':ACTIVE:', '-e', '0,%s,%s,%s,%s' % (
+            p[0]-OFFSET[0],
+            p[1]-OFFSET[1]+s[1]/2,
+            s[0],s[1]/2
+        )
+    ])
+elif sys.argv[1]=="hsplit":
+    p = active_window.pos
+    s = active_window.size
+    subprocess.call([
+        'wmctrl', '-r', ':ACTIVE:', '-e', '0,%s,%s,%s,%s' % (
+            p[0]-OFFSET[0],
+            p[1]-OFFSET[1],
+            s[0]/2,s[1]
+        )
+    ])
+    time.sleep(DELAY)
+    subprocess.Popen([sys.argv[2]])
+    update()
+    time.sleep(DELAY)
+    subprocess.call([
+        'wmctrl', '-r', ':ACTIVE:', '-e', '0,%s,%s,%s,%s' % (
+            p[0]-OFFSET[0]+s[0]/2,
+            p[1]-OFFSET[1],
+            s[0]/2,s[1]
+        )
+    ])
+
 
